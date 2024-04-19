1b11253c3567952b279b20550de42633fc8daa3a
nikita3006 <nikitanegi3011@gmail.com>
2024-04-19 17:34:03 +0530

    button from mui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Button} from '@mui/material'
+
+const App = () => {
+  return (
+    <div>
+      <h1>REact UI</h1>
+      <button>send me</button>
+      <Button variant="contained">Contained</Button>
+    </div>
+  )
+}
+
+export default App
